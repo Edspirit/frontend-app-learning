81c83aa88c803ba82ed3a38ef9bffac990a27c43
Khaled <info@khaledr.ir>
2023-11-26 19:43:47 +0330

    fix: don't show courses to unregistered users

--- src/alerts/enrollment-alert/EnrollmentAlert.jsx
+++ src/alerts/enrollment-alert/EnrollmentAlert.jsx
@@ -10,6 +10,7 @@ import { useModel } from '../../generic/model-store';
 
 import messages from './messages';
 import useEnrollClickHandler from './clickHook';
+import useUserData from './useUserData';
 
 const EnrollmentAlert = ({ intl, payload }) => {
   const {
@@ -28,6 +29,7 @@ const EnrollmentAlert = ({ intl, payload }) => {
     org,
     intl.formatMessage(messages.success),
   );
+  const { userData } = useUserData();
 
   let text = intl.formatMessage(messages.alert);
   let type = 'warning';
@@ -46,12 +48,19 @@ const EnrollmentAlert = ({ intl, payload }) => {
     </Button>
   );
 
-  return (
-    <Alert variant={type} icon={icon}>
-      <div className="d-flex">
+  const alertContent = userData?.is_active
+    ? (
+      <>
         {text}
         {button}
         {loading && <FontAwesomeIcon icon={faSpinner} spin />}
+      </>
+    )
+    : intl.formatMessage(messages.deActive);
+  return (
+    <Alert variant={type} icon={icon}>
+      <div className="d-flex">
+        {alertContent}
       </div>
     </Alert>
   );
